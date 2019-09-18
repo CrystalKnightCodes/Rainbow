519a8226e82e03d12233582eee3fbe2ebf23e50d
CrystalRainn <CrystalRainn@icloud.com>
2019-09-17 22:55:14 -0400

    Added Colors

--- Rainbow/View Controllers/ColorsTableViewController.swift
+++ Rainbow/View Controllers/ColorsTableViewController.swift
@@ -9,13 +9,24 @@
 import UIKit
 
 class ColorsTableViewController: UITableViewController {
+    var colors: [Color] = [
+        Color(name: "Red", color: .red),
+        Color(name: "Orange", color: .orange),
+        Color(name: "Yellow", color: .yellow),
+        Color(name: "Green", color: .green),
+        Color(name: "Blue", color: .blue),
+        Color(name: "Cyan", color: .cyan),
+        Color(name: "Purple", color: .purple)
+    ]
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return colors.count
     }
 
 override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "ColorCell", for: indexPath)
-    cell.textLabel?.text = "Cell #\(indexPath).row)"
+// let color = colors[indexPath.row]
+   // cell.textLabel.text = color
     return cell
 }
 }
