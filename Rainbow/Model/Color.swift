6cb7f3b85da3deaf81e7575cbf4afdbb1b59661d
CrystalRainn <CrystalRainn@icloud.com>
2019-09-17 22:44:33 -0400

    Added Color Model

---
+++ Rainbow/Model/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Rainbow
+//
+//  Created by Christy Hicks on 9/17/19.
+//  Copyright © 2019 Knight Night. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let color: UIColor
+}
--- Rainbow/View Controllers/ColorsTableViewController.swift
+++ Rainbow/View Controllers/ColorsTableViewController.swift
@@ -9,5 +9,13 @@
 import UIKit
 
 class ColorsTableViewController: UITableViewController {
-    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+
+override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "ColorCell", for: indexPath)
+    cell.textLabel?.text = "Cell #\(indexPath).row)"
+    return cell
+}
 }
